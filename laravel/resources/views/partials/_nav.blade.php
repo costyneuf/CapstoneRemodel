a91ea73f424ad2dcfb7c41c3369061b663231850
Xing Gao <youzi_g@163.com>
2018-06-09 14:26:52 -0400

    add home page

---
+++ laravel/resources/views/partials/_nav.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar">
+    <div class="container">
+        <a class="navbar-brand" href="https://wexnermedical.osu.edu">
+            <img src="https://wexnermedical.osu.edu/-/media/images/wexnermedical/global/modules/global/header/osuwexmedctr.png?la=en&hash=E40538F4EC98B54A105B79EC8FA1BAB09709DDDC" height="40" alt="">
+        </a>
+    </div>
+</nav>
+<nav class="navbar navbar-expand-lg" style="background-color: #bb0000;">
+    <div class="container">
+        <a class="navbar-brand" href="#">REMODEL</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about">About Me</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact">Contact Me</a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<br>
