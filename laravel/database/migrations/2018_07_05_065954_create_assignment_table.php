49c5394fc35e630863ac0c3c666c259474a18acd
Xing Gao <youzi_g@163.com>
2018-07-05 03:33:36 -0400

    add shibboleth files

--- laravel/database/migrations/2018_07_05_065954_create_assignment_table.php
+++ laravel/database/migrations/2018_07_05_065954_create_assignment_table.php
@@ -17,7 +17,7 @@ class CreateAssignmentTable extends Migration
             $table->increments('id');
             $table->date('date');
             $table->unsignedInteger('resident');
-            $table->unsignedInteger('schedule');            
+            $table->unsignedInteger('schedule')->unique();            
             $table->unsignedInteger('attending');        
             $table->timestamps();
         });
