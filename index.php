b717c6a7e2c5022b35069bd3402ebfed3d953ea8
Xing Gao <youzi_g@163.com>
2018-07-05 04:15:14 -0400

    change directory in index

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <html>
 <head>
 <title>A web page that points a browser to a different page after 2 seconds</title>
-<meta http-equiv="refresh" content="0; URL=/laravel/blog/public">
+<meta http-equiv="refresh" content="0; URL=/laravel/public">
 <meta name="keywords" content="automatic redirection">
 </head>
 <body>
 If your browser doesn't automatically go there within a few seconds, 
 you may want to go to 
-<a href="/laravel/blog/public">the destination</a> 
+<a href="/laravel/public">the destination</a> 
 manually.
 </body>
 </html>
