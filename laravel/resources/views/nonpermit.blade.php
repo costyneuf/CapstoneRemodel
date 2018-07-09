ff05ac0021ee8e5e4b673d608b1ea36674a72e90
Xing Gao <youzi_g@163.com>
2018-07-09 00:22:11 -0400

    add no permission page

---
+++ laravel/resources/views/nonpermit.blade.php
@@ -0,0 +1,5 @@
+@extends('main')
+@section('content')
+    <h1>You do not have the permission to visit this page!</h1>
+    <a href="/laravel/public">click me to the home page</a>
+@endsection
--- laravel/resources/views/partials/_nav.blade.php
+++ laravel/resources/views/partials/_nav.blade.php
@@ -17,23 +17,32 @@
                     <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/about" id="about">Personal Information</a>
+                    <a class="nav-link" href="/laravel/public/about" id="about">Personal Information</a>
                     <script>
-                        document.getElementById("about").href = window.location.pathname + "/about";
+                        // document.getElementById("about").href = window.location.pathname + "/about";
                         //if (window.location.pathname == "/")
                         //{
                         //    document.getElementById("return").style.visibility = "hidden";
                         //}
+                        if (window.location.pathname.includes("resident"))
+                        {
+                            document.getElementById("about").href = "/laravel/public/resident/about";
+                        } else if (window.location.pathname.includes("admin"))
+                        {
+                            document.getElementById("about").href = "/laravel/public/admin/about";
+                        }
                     </script> 
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/contact" id="contact">Contact Us</a>
+                    <a class="nav-link" href="/laravel/public/contact" id="contact">Contact Us</a>
                     <script>
-                            document.getElementById("contact").href = window.location.pathname + "/contact";
-                            //if (window.location.pathname == "/")
-                            //{
-                            //    document.getElementById("return").style.visibility = "hidden";
-                            //}
+                            if (window.location.pathname.includes("resident"))
+                            {
+                                document.getElementById("contact").href = "/laravel/public/resident/contact";
+                            } else if (window.location.pathname.includes("admin"))
+                            {
+                                document.getElementById("contact").href = "/laravel/public/admin/contact";
+                            }
                     </script> 
                 </li>
             </ul>
