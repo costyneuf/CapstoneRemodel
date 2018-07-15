66dbd76b42486112d0b5011514db207f597df64c
costyneuf <youzi_g@163.com>
2018-07-15 14:42:43 -0400

    update migration setup

--- laravel/database/migrations/2018_07_05_065235_create_admin_table.php
+++ laravel/database/migrations/2018_07_05_065235_create_admin_table.php
@@ -3,9 +3,83 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateAdminTable extends Migration
 {
+    /**
+     * Initialize data in the table.
+     *
+     * @return void
+     */
+    private function initialize()
+    {
+        if (file_exists ( $_ENV["BACKUP_PATH"]."admin.csv" )) {
+
+            /**
+             * Read data from the backup file and add into database
+             */
+            $fp = fopen($_ENV["BACKUP_PATH"]."admin.csv", 'r');
+            
+            // Read the first row
+            fgetcsv($fp);
+
+            // Read rows until null
+            while (($line = fgetcsv($fp)) !== false)
+            {
+                $name = $line[0];
+                $email = $line[1];    
+                DB::table('admin')->insert(
+                    ['name' => $name, 'email' => $email]
+                );
+            }
+
+            // Close file
+            fclose($fp);
+
+            return;
+        }
+
+        // Insert the primary admin if backup file does not exist
+        DB::table('admin')->insert([
+            'email' => $_ENV["ADMIN_PRIMARY_EMAIL"], 
+            'name' => $_ENV["ADMIN_PRIMARY_NAME"]    
+        ]);
+    }
+
+    /**
+     * Backup data in the table.
+     *
+     * @return void
+     */
+    private function backup()
+    {
+        /** 
+         * Save data sets into a csv file
+         */        
+        $filename = $_ENV["BACKUP_PATH"]."admin.csv";
+        $data = DB::table('admin')->get();
+        
+        // Erase existing file
+        $output = fopen($filename, 'w+');
+        // Set up the first row
+        fputcsv($output, array(
+            'name', 
+            'email'
+        ));
+        // Add all rows
+        foreach ($data as $info) {
+            fputcsv($output, array(
+                $info['name'],
+                $info['email']
+            ));
+        }
+
+        // Close file
+        fclose($output);
+    
+    }
+
     /**
      * Run the migrations.
      *
@@ -14,11 +88,15 @@ class CreateAdminTable extends Migration
     public function up()
     {
         Schema::create('admin', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name'); // Name of the admin
+            $table->string('email')->unique()->primary(); // Primary Key: email of the admin
+
+            // Add for future extension
             $table->timestamps();
         });
+
+        self::initialize();
+
     }
 
     /**
@@ -28,6 +106,9 @@ class CreateAdminTable extends Migration
      */
     public function down()
     {
+
+        self::backup();
+
         Schema::dropIfExists('admin');
     }
 }
